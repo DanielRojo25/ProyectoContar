867685f0f6b6b1b0ba6edc1459e04c7d854e2ee3
drojo <drojo2508@gmail.com>
2022-11-12 19:44:04 -0700

    Hola

--- app/src/main/java/com/example/proyectocontar/MainActivity.kt
+++ app/src/main/java/com/example/proyectocontar/MainActivity.kt
@@ -6,79 +6,41 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.example.proyectocontar.databinding.ActivityMainBinding
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.lang.Thread.sleep
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
-    lateinit var numeros : HiloNumeros
     lateinit var mp : MediaPlayer
-    var bandera = false
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        numeros = HiloNumeros(this)
-        setContentView(binding.root)
-
-        binding.btnIniciar.setOnClickListener {
-            try {
-                numeros.start()
-            }catch (e:Exception){
-                Toast.makeText(this,"Solo se puede iniciar una vez!",Toast.LENGTH_SHORT).show()
-            }
-
-        }
-
-        binding.btnPausa.setOnClickListener {
-            cambiarTexto()
-
-        }
-
-        binding.btnTerminar.setOnClickListener {
-            numeros.terminar()
-        }
-
-    }
-
-    fun cambiarTexto (){
-        if (bandera){
-            binding.btnPausa.text="CONTINUAR"
-            numeros.pausar()
-
-        }else{
-            binding.btnPausa.text="PAUSA"
-            numeros.pausar()
-        }
-        bandera = !bandera
-    }
-}
-
-class HiloNumeros(puntero:MainActivity):Thread(){
-    var p = puntero
 
     var iniciar = true
     var pausar  = false
     var indice = -1
+    var bandera = false
 
     var cartasNumeros = arrayListOf(R.drawable.uno, R.drawable.dos, R.drawable.tres, R.drawable.cuatro, R.drawable.cinco,
-                                    R.drawable.seis, R.drawable.siete, R.drawable.ocho, R.drawable.nueve, R.drawable.diez,
-                                    R.drawable.once, R.drawable.doce, R.drawable.trece, R.drawable.catorce, R.drawable.quince,
-                                    R.drawable.dieciseis, R.drawable.diecisiete, R.drawable.dieciocho, R.drawable.diecinueve, R.drawable.veinte,
-                                    R.drawable.veintiuno, R.drawable.veintidos, R.drawable.veintitres, R.drawable.veinticuatro, R.drawable.veinticinco,
-                                    R.drawable.veintiseis, R.drawable.veintisiete, R.drawable.veintiocho, R.drawable.veintinueve, R.drawable.treinta,
-                                    R.drawable.treintayuno, R.drawable.treintaydos, R.drawable.treintaytres, R.drawable.treintaycuatro, R.drawable.treintaycinco,
-                                    R.drawable.treintayseis, R.drawable.treintaysiete, R.drawable.treintayocho, R.drawable.treintaynueve, R.drawable.cuarenta,
-                                    R.drawable.cuarentayuno, R.drawable.cuarentaydos, R.drawable.cuarentaytres, R.drawable.cuarentaycuatro, R.drawable.cuarentaycinco,
-                                    R.drawable.cuarentayseis, R.drawable.cuarentaysiete, R.drawable.cuarentayocho, R.drawable.cuarentaynueve, R.drawable.cincuenta,
-                                    R.drawable.cincuentayuno, R.drawable.cincuentaydos, R.drawable.cincuentaytres, R.drawable.cincuentaycuatro, R.drawable.cincuentaycinco,
-                                    R.drawable.cincuentayseis, R.drawable.cincuentaysiete, R.drawable.cincuentayocho, R.drawable.cincuentaynueve, R.drawable.sesenta,
-                                    R.drawable.sesentayuno, R.drawable.sesentaydos, R.drawable.sesentaytres, R.drawable.sesentaycuatro, R.drawable.sesentaycinco,
-                                    R.drawable.sesentayseis, R.drawable.sesentaysiete, R.drawable.sesentayocho, R.drawable.sesentaynueve, R.drawable.setenta,
-                                    R.drawable.setentayuno, R.drawable.setentaydos, R.drawable.setentaytres, R.drawable.setentaycuatro, R.drawable.setentaycinco,
-                                    R.drawable.setentayseis, R.drawable.setentaysiete, R.drawable.setentayocho, R.drawable.setentaynueve, R.drawable.ochenta,
-                                    R.drawable.ochentayuno, R.drawable.ochentaydos, R.drawable.ochentaytres, R.drawable.ochentaycuatro, R.drawable.ochentaycinco,
-                                    R.drawable.ochentayseis, R.drawable.ochentaysiete, R.drawable.ochentayocho, R.drawable.ochentaynueve, R.drawable.noventa,
-                                    R.drawable.noventayuno, R.drawable.noventaydos, R.drawable.noventaytres, R.drawable.noventaycuatro, R.drawable.noventaycinco,
-                                    R.drawable.noventayseis, R.drawable.noventaysiete, R.drawable.noventayocho, R.drawable.noventaynueve, R.drawable.cien)
+        R.drawable.seis, R.drawable.siete, R.drawable.ocho, R.drawable.nueve, R.drawable.diez,
+        R.drawable.once, R.drawable.doce, R.drawable.trece, R.drawable.catorce, R.drawable.quince,
+        R.drawable.dieciseis, R.drawable.diecisiete, R.drawable.dieciocho, R.drawable.diecinueve, R.drawable.veinte,
+        R.drawable.veintiuno, R.drawable.veintidos, R.drawable.veintitres, R.drawable.veinticuatro, R.drawable.veinticinco,
+        R.drawable.veintiseis, R.drawable.veintisiete, R.drawable.veintiocho, R.drawable.veintinueve, R.drawable.treinta,
+        R.drawable.treintayuno, R.drawable.treintaydos, R.drawable.treintaytres, R.drawable.treintaycuatro, R.drawable.treintaycinco,
+        R.drawable.treintayseis, R.drawable.treintaysiete, R.drawable.treintayocho, R.drawable.treintaynueve, R.drawable.cuarenta,
+        R.drawable.cuarentayuno, R.drawable.cuarentaydos, R.drawable.cuarentaytres, R.drawable.cuarentaycuatro, R.drawable.cuarentaycinco,
+        R.drawable.cuarentayseis, R.drawable.cuarentaysiete, R.drawable.cuarentayocho, R.drawable.cuarentaynueve, R.drawable.cincuenta,
+        R.drawable.cincuentayuno, R.drawable.cincuentaydos, R.drawable.cincuentaytres, R.drawable.cincuentaycuatro, R.drawable.cincuentaycinco,
+        R.drawable.cincuentayseis, R.drawable.cincuentaysiete, R.drawable.cincuentayocho, R.drawable.cincuentaynueve, R.drawable.sesenta,
+        R.drawable.sesentayuno, R.drawable.sesentaydos, R.drawable.sesentaytres, R.drawable.sesentaycuatro, R.drawable.sesentaycinco,
+        R.drawable.sesentayseis, R.drawable.sesentaysiete, R.drawable.sesentayocho, R.drawable.sesentaynueve, R.drawable.setenta,
+        R.drawable.setentayuno, R.drawable.setentaydos, R.drawable.setentaytres, R.drawable.setentaycuatro, R.drawable.setentaycinco,
+        R.drawable.setentayseis, R.drawable.setentaysiete, R.drawable.setentayocho, R.drawable.setentaynueve, R.drawable.ochenta,
+        R.drawable.ochentayuno, R.drawable.ochentaydos, R.drawable.ochentaytres, R.drawable.ochentaycuatro, R.drawable.ochentaycinco,
+        R.drawable.ochentayseis, R.drawable.ochentaysiete, R.drawable.ochentayocho, R.drawable.ochentaynueve, R.drawable.noventa,
+        R.drawable.noventayuno, R.drawable.noventaydos, R.drawable.noventaytres, R.drawable.noventaycuatro, R.drawable.noventaycinco,
+        R.drawable.noventayseis, R.drawable.noventaysiete, R.drawable.noventayocho, R.drawable.noventaynueve, R.drawable.cien)
 
 
     var audiosNumeros = arrayListOf(R.raw.uno, R.raw.dos, R.raw.tres, R.raw.cuatro, R.raw.cinco,
@@ -102,51 +64,77 @@ class HiloNumeros(puntero:MainActivity):Thread(){
         R.raw.noventayuno, R.raw.noventaydos, R.raw.noventaytres, R.raw.noventaycuatro, R.raw.noventaycinco,
         R.raw.noventayseis, R.raw.noventaysiete, R.raw.noventayocho, R.raw.noventaynueve, R.raw.cien)
 
+    fun corrutina() = GlobalScope.launch {
+        while (iniciar){
+
+            if(!pausar){
+
+                runOnUiThread{
+                    recorrerNumeros()
+                }
+
+                delay(1000)
+
+            }
+
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnIniciar.setOnClickListener {
+            corrutina()
+            binding.btnIniciar.isClickable=false
+        }
+
+        binding.btnPausa.setOnClickListener {
+            cambiarTexto()
+
+        }
+
+        binding.btnTerminar.setOnClickListener {
+            terminar()
+        }
+
+    }
+
     fun recorrerNumeros(){
 
         indice++
         if (indice == cartasNumeros.size) indice = 99
-        p.binding.imagen.setImageResource(cartasNumeros[indice])
+        binding.imagen.setImageResource(cartasNumeros[indice])
 
-        p.mp = MediaPlayer.create(p,audiosNumeros[indice])
-        p.mp.start()
+        mp = MediaPlayer.create(this,audiosNumeros[indice])
+        mp.start()
 
-        if(indice==99){
-            p.runOnUiThread{
-                p.binding.texto.text="Juego Terminado!"
-            }
+        if(indice==99) {
+            mp.stop()
+            binding.texto.text = "Juego Terminado!"
         }
-
-        sleep(1000)
-
     }
 
     fun terminar(){
         iniciar = false
-        p.mp.stop()
-        p.binding.imagen.setImageResource(R.drawable.numeros)
+        mp.stop()
+        binding.imagen.setImageResource(R.drawable.numeros)
     }
 
     fun pausar(){
         pausar = pausar == false
     }
 
-    override fun run() {
-        super.run()
+    fun cambiarTexto (){
+        if (bandera){
+            binding.btnPausa.text="PAUSA"
+            pausar()
 
-        while (iniciar){
-
-            if(!pausar){
-
-                p.runOnUiThread{
-                    recorrerNumeros()
-                }
-
-                sleep(1000)
-
-            }
-
+        }else{
+            binding.btnPausa.text="CONTINUAR"
+            pausar()
         }
+        bandera = !bandera
     }
-
 }
